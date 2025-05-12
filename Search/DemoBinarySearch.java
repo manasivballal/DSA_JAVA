89107585fa46ff4b619b4a4c2cb52842595379c4
Manasi Ballal <ballal.manasivikas2024e@vitstudent.ac.in>
2025-05-12 11:23:32 +0530

    Add files via upload

---
+++ Search/DemoBinarySearch.java
@@ -0,0 +1,15 @@
+package com.binarysearch;
+
+public class DemoBinarySearch {
+    public static void main(String[] args) {
+        // 2 pointers are required
+        // array should be sorted for that
+        // (start+end)/2
+        // check whether the target is equal ,greater or lesser and act accordingly .
+        // as size increases time is constant.
+        // Q.why BINARY SEARCH ?
+        //->>>>>N , N/2 , N/4 , N/8  ......N/2^K
+
+    }
+}
+ 
